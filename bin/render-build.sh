215da835adb95f0e867b8db18b675f467434eeaf
FURUKAWA Tomoya <116810310+fulltomo@users.noreply.github.com>
2024-08-23 19:33:58 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,5 +11,5 @@ bundle exec rails assets:clean
 # Uncomment the following line:
 
 # bundle exec rails db:migrate
-bundle exac rails db:seed
-bundle exac rails db:migrate
+bundle exec rails db:seed
+bundle exec rails db:migrate
